84079612bcdbe667b870ecf385ec506822317cdc
Eric Koncina <mail@koncina.eu>
2020-12-05 12:34:47 +0100

    first commit

---
+++ R/paths.R
@@ -0,0 +1,12 @@
+get_rel_path <- function() {
+  h <- here()
+  h <- path_split(h)[[1]]
+  h <- h[(which(h == "projects") + 1):length(h)]
+  path_join(h)
+}
+
+shared_path <- function(share_folder, filename) {
+  f <- file.path(user_data_dir(share_folder), filename)
+  dir_create(dirname(f))
+  f
+}
---
+++ R/reports.R
@@ -0,0 +1,37 @@
+NULL
+
+#' Knit report within a targets pipeline
+#'
+#' Knit the report and store the rendered file in the shared output folder.
+#' Adapted from Aurélien's \href{https://git-r3lab.uni.lu/aurelien.ginolhac/rworkshop}{rworkshop repository code} ()
+#'
+#' @importFrom rmarkdown render
+#' @importFrom here here
+#' @importFrom fs file_move
+#' @importFrom callr r
+#' @importFrom targets tar_cancel
+#'
+#' @param rmd The Rmd file to be rendered
+#' @param output_format The rmarkdown output format to be used
+#'
+#' @export
+knit_report <- function(rmd, output_format) {
+  tar_cancel(is.null(rmd))
+  rmd_date <- created(rmd)
+  message("Knitting ", rmd_date, "-", basename(rmd))
+  if (!file.exists(rmd)) stop("Rmd file doesn't exist!", call. = FALSE)
+
+  # callr by gabenbuie https://github.com/rstudio/gt/issues/297#issuecomment-497778735
+  output_file <- callr::r(function(...) rmarkdown::render(...),
+                          args = list(input = rmd,
+                                      output_format = output_format,
+                                      quiet = FALSE,
+                                      knit_root_dir = here::here())
+  )
+
+  file_move(output_file, shared_path("reports", file.path(get_rel_path(),
+                                                          paste0(rmd_date,
+                                                                 "-",
+                                                                 basename(output_file)))))
+}
+
