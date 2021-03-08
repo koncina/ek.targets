835fbe3d7e08ce0c65e5399dbb78c6096b4a2334
Eric Koncina <mail@koncina.eu>
2021-03-08 10:41:13 +0100

    add new function to detect projects root similar to here

--- R/paths.R
+++ R/paths.R
@@ -5,6 +5,18 @@ get_rel_path <- function() {
   path_join(h)
 }
 
+#' @export
+rel_project <- function(marker_file = ".projects", from = TRUE) {
+  x <- accumulate(path_split(getwd())[[1]], file.path)
+
+  if (isTRUE(from)) {
+    path_rel(x[length(x)],
+             x[max(which(file_exists(file.path(x, ".projects"))))])
+  } else {
+    x[max(which(file_exists(file.path(x, ".projects"))))]
+  }
+}
+
 shared_path <- function(share_folder, filename) {
   f <- file.path(user_data_dir(share_folder), filename)
   dir_create(dirname(f))
