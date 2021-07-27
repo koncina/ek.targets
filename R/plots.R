14dc533e80e03a91f73915cfe2545666050725f2
Eric Koncina <mail@koncina.eu>
2021-07-27 16:18:01 +0200

    determine optimal heatmap size during saving

--- R/plots.R
+++ R/plots.R
@@ -14,6 +14,16 @@ write_plot <- function(x, filename, ...) {
   f
 }
 
+get_heatmap_size <- function(ht, units = "px", ...) {
+  ht <- draw(ht, ...)
+  w  <-  ComplexHeatmap:::width(ht)
+  w <-  convertX(w, units, valueOnly = TRUE)
+  h <-  ComplexHeatmap:::height(ht)
+  h <-  convertY(h, units, valueOnly = TRUE)
+  list(width = w,
+       height = h)
+}
+
 #' Save a ComplexHeatmap as a standalone png file
 #'
 #' @importFrom ragg agg_png
@@ -24,10 +34,17 @@ write_plot <- function(x, filename, ...) {
 #' @param width,height,units,res The settings for the output file
 #'
 #' @export
-write_heatmap_png <- function(x, filename, width = 500, height = 500, units = "px", res = 150, ...) {
+write_heatmap_png <- function(x, filename, ..., width = NA, height = NA, units = "mm", res = 150) {
   stopifnot(str_detect(filename, "\\.png$"))
   f <- shared_path("reports", file.path(get_rel_path(remove = 0), "plots", filename))
   dir_create(dirname(f))
+
+  if (is.na(width) | is.na(height)) {
+    ht_size <- get_heatmap_size(x, units = units, ...)
+    width <- ht_size$width
+    height <- ht_size$height
+  }
+
   agg_png(f, width = width, height = height, units = units, res = res)
   draw(x, ...)
   invisible(dev.off())
@@ -44,12 +61,19 @@ write_heatmap_png <- function(x, filename, width = 500, height = 500, units = "p
 #' @param width,height The settings for the output file
 #'
 #' @export
-write_heatmap <- function(x, filename, width = 8, height = 8) {
+write_heatmap <- function(x, filename, ..., width = NA, height = NA) {
   stopifnot(str_detect(filename, "\\.pdf$"))
   f <- shared_path("reports", file.path(get_rel_path(remove = 0), "plots", filename))
   dir_create(dirname(f))
+
+  if (is.na(width) | is.na(height)) {
+    ht_size <- get_heatmap_size(x, units = "inches", ...)
+    width <- ht_size$width
+    height <- ht_size$height
+  }
+
   pdf(f, width = width, height = height)
-  draw(x)
+  draw(x, ...)
   dev.off()
   f
 }
